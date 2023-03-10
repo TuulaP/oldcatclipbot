b92a45003c1ac7a4f678e5620fbbbb7509b7cb07
TuulaP <TuulaP@users.noreply.github.com>
2023-03-10 18:29:05 +0200

    Added dotenv, TODO to find some mastodont library

--- oldcatclipbot.py
+++ oldcatclipbot.py
@@ -3,32 +3,23 @@
 
 import random
 import requests
+#from mastodon import Mastodon  - some buffer error?
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 baseserver = "https://digi.kansalliskirjasto.fi"
 ## local = "http://localhost:9090"
 
 api_url = baseserver + "/rest/article-search/search-by-type?offset=0&count=22"
-
-
-#from mastodon import Mastodon
-
-
-#Mastodon.create_app(
-#    'oldcatclipbot',
-#    api_base_url = 'https://mastodon.social',
-#    to_file = 'pytooter_clientcred.secret'
-#)
-
-
 headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-
 data = {
     "keywords": ['kissa']
 }
 
 
 r = requests.post(url=api_url, json=data, headers=headers)  
-
 
 print("Status:", r.status_code)
 
@@ -57,6 +48,19 @@ if (r.status_code == 200):
     tootText += "\nSee full image at: "+fullUrl
 
     print(tootText)
+
+    # Mastodon.create_app(
+    #     'oldcatclipbot',
+    #     api_base_url='https://botsin.space',
+    #     to_file=os.environ(APPSECRET)  
+    # )
+
+    # mastodon = Mastodon(client_id = os.environ(APPSECRET),)
+    # mastodon.log_in(os.environ(BOTEMAIL),
+    #     os.environ(BOTPWD),
+    # to_file = os.environ(APPSECRET)
+
+#)
 
 
 
